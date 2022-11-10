bbfdc1497f1ba87f4dbf390848e17edd0c7c484b
Andrew Sampson <andrew@akliz.net>
2022-11-09 22:35:52 -0600

    Make Dissolver output try to stack in existing slots first.

--- src/main/java/com/smashingmods/alchemistry/common/block/dissolver/DissolverBlockEntity.java
+++ src/main/java/com/smashingmods/alchemistry/common/block/dissolver/DissolverBlockEntity.java
@@ -22,6 +22,7 @@ import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.items.ItemHandlerHelper;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -101,7 +102,7 @@ public class DissolverBlockEntity extends AbstractInventoryBlockEntity {
             for (int j = 0; j < getOutputHandler().getStacks().size(); j++) {
                 ItemStack slotStack = getOutputHandler().getStackInSlot(j).copy();
                 if (slotStack.isEmpty() || (ItemStack.isSameItemSameTags(bufferStack, slotStack) && bufferStack.getCount() + slotStack.getCount() <= slotStack.getMaxStackSize())) {
-                    getOutputHandler().setOrIncrement(j, bufferStack);
+                    ItemHandlerHelper.insertItemStacked(getOutputHandler(), bufferStack, false);
                     internalBuffer.remove(i);
                     break;
                 }
@@ -168,9 +169,21 @@ public class DissolverBlockEntity extends AbstractInventoryBlockEntity {
     @Override
     public ProcessingSlotHandler initializeOutputHandler() {
         return new ProcessingSlotHandler(12) {
+
+            private boolean valid = false;
+
+            @NotNull
+            @Override
+            public ItemStack insertItem(int slot, @NotNull ItemStack stack, boolean simulate) {
+                valid = true;
+                ItemStack itemStack = super.insertItem(slot, stack, simulate);
+                valid = false;
+                return itemStack;
+            }
+
             @Override
             public boolean isItemValid(int pSlot, ItemStack pItemStack) {
-                return false;
+                return valid;
             }
 
             @Override
